3133e00c133d875c287bd7eef49479a15e0454d8
Hemant Vishwakarma <hemantvishwakarmact19@acropolis.in>
2022-03-10 15:29:50 +0530

    Update FacultyRegister.java

--- app/src/main/java/com/example/minorproject/loginandregister/FacultyRegister.java
+++ app/src/main/java/com/example/minorproject/loginandregister/FacultyRegister.java
@@ -23,6 +23,9 @@ public class FacultyRegister extends Fragment {
 TextView loginFac;
 Button registerFac;
 EditText pass;
+    FirebaseAuth auth;
+    FragmentFacultyRegisterBinding binding;
+    FirebaseDatabase database;
 
     public FacultyRegister() {
         // Required empty public constructor
@@ -34,29 +37,88 @@ EditText pass;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_faculty_register, container, false);
-        loginFac = view.findViewById(R.id.LoginFaculty);
-        registerFac = view.findViewById(R.id.FacultyRegister);
-        pass = view.findViewById(R.id.editTextTextPasswordFac);
-        loginFac.setOnClickListener(new View.OnClickListener() {
+        binding = FragmentFacultyRegisterBinding.inflate(inflater,container,false);
+        View view =  binding.getRoot();
+
+        auth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance();
+
+
+        ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setTitle("Creating Account....");
+        progressDialog.setMessage("We are creating your account....");
+        progressDialog.dismiss();
+
+        binding.loginFaculty.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent  = new Intent(getContext(),FacultyLoginActivity.class);
                 startActivity(intent);
             }
         });
-        registerFac.setOnClickListener(new View.OnClickListener() {
+        binding.FacultyRegisterbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                    Intent intent = new Intent(getContext(), HomePageActivityForFaculty.class);
-                    startActivity(intent);
+                progressDialog.show();
 
 
+                auth.createUserWithEmailAndPassword(binding.fctEmailId.getText().toString(),binding.fctpassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            String fctuserId = task.getResult().getUser().getUid();
+                           FacultyModel facultyModel = new FacultyModel(binding.fctEmailId.getText().toString(),binding.fctpassword.getText().toString(),binding.fctUserName.getText().toString());
+                            database.getReference().child("FacultyData").child(fctuserId).setValue(facultyModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(getContext(), "Success", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getContext(), HomePageActivityForFaculty.class);
+                                        startActivity(intent);
+                                        progressDialog.dismiss();
+                                    }
+                                    else {
+                                        Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
 
 
+
+
+
+                        }
+                        else{
+
+                        }
+                    }
+                });
             }
         });
+
+//        loginFac = view.findViewById(R.id.LoginFaculty);
+//        registerFac = view.findViewById(R.id.FacultyRegisterbtn);
+//        pass = view.findViewById(R.id.fctpassword);
+//        loginFac.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent  = new Intent(getContext(),FacultyLoginActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//        registerFac.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                    Intent intent = new Intent(getContext(), HomePageActivityForFaculty.class);
+//                    startActivity(intent);
+//
+//
+//
+//
+//            }
+//        });
         return  view;
     }
 }
