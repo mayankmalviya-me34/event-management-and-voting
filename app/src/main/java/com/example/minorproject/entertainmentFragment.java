c72fed5dd1a569e50057d17854eaaea406440448
Hemant Vishwakarma <hemantvishwakarmact19@acropolis.in>
2022-04-17 10:41:33 +0530

    show information dialog

--- app/src/main/java/com/example/minorproject/entertainmentFragment.java
+++ app/src/main/java/com/example/minorproject/entertainmentFragment.java
@@ -10,7 +10,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class entertainmentFragment extends Fragment {
-    Button create;
+     Button editbtn,saveInfobtn;
+    TextView textView;
+    Dialog dialog;
 
 
 
@@ -26,6 +28,30 @@ public class entertainmentFragment extends Fragment {
 
        View view =  inflater.inflate(R.layout.fragment_entertainment, container, false);
        //create = view.findViewById(R.id.CreateEvtButton);
+        
+          textView = view.findViewById(R.id.semi);
+
+
+
+        editbtn = view.findViewById(R.id.button);
+        editbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v," hi",Snackbar.LENGTH_LONG).show();
+                dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.student_info_form);
+                Window window = dialog.getWindow();
+                window.setLayout(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+
+                dialog.setCancelable(true);
+
+                dialog.show();
+            }
+        });
+
+
+
+        textView.setText(" hiiiii");
        return  view;
     }
 }
